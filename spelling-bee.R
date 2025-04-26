cf10caab60b455c601a34a5accefc81e464f60b4
Colin Hill <colinhill@Colins-MacBook-Air.local>
2025-04-26 16:13:15 -0400

    Added in checks to make sure there are enough valid words and at least one pangram

--- spelling-bee.R
+++ spelling-bee.R
@@ -13,12 +13,26 @@ allwords <- read.delim('~/Documents/spelling-bee/words_alpha.txt',
          # create subset of words that are pangrams
          pangram = unique_char == 7)
 
-# select 7 random letters and pick one as the middle letter
-letterlist <- sample(letters, 7, replace = F)
-middle <- letterlist[1]
+# initialize counters of valid words and pangrams - resample if too low
+validwords <- 0
+pangrams <- 0
+i <- 1
 
-# create list of possible words
-solutionlist <- allwords %>%
-  filter(valid) %>%
-  filter(sum(!(str_split_1(word, '') %in% letterlist)) == 0) %>%
-  filter(grepl(middle, word)) 
+while (validwords < 10 | pangrams == 0) {
+  print(paste0('i =  ', i))
+  # select 7 random letters and pick one as the middle letter
+  letterlist <- sample(letters, 7, replace = F)
+  middle <- letterlist[1]
+  
+  # create list of possible words
+  solutionlist <- allwords %>%
+    filter(valid) %>%
+    filter(sum(!(str_split_1(word, '') %in% letterlist)) == 0) %>%
+    filter(grepl(middle, word)) 
+  
+  validwords <- nrow(solutionlist)
+  pangrams <- sum(solutionlist$pangram)
+  print(validwords)
+  print(pangrams)
+  i <- i + 1
+}
