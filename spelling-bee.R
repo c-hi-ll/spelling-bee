3766b3783580351a70eb82d5194de0e54123c53a
Colin Hill <colinhill@Colins-MacBook-Air.local>
2025-04-26 16:23:21 -0400

    Guaranteeing at least one vowel in letter list

--- spelling-bee.R
+++ spelling-bee.R
@@ -13,26 +13,31 @@ allwords <- read.delim('~/Documents/spelling-bee/words_alpha.txt',
          # create subset of words that are pangrams
          pangram = unique_char == 7)
 
+validwords <- allwords %>%
+  filter(valid)
+
 # initialize counters of valid words and pangrams - resample if too low
-validwords <- 0
-pangrams <- 0
+validwordcount <- 0
+pangramcount <- 0
 i <- 1
 
-while (validwords < 10 | pangrams == 0) {
+while (validwordcount < 10 | pangramcount == 0) {
   print(paste0('i =  ', i))
   # select 7 random letters and pick one as the middle letter
-  letterlist <- sample(letters, 7, replace = F)
-  middle <- letterlist[1]
+  # make sure at least one is a vowel
+  vowel <- sample(c('a', 'e', 'i', 'o', 'u'), 1)
+  letterlist <- c(vowel, sample(letters[letters != vowel], 6, replace = F))
+  middle <- letterlist[sample(c(1:7), 1)]
   
   # create list of possible words
-  solutionlist <- allwords %>%
+  solutionlist <- validwords %>%
     filter(valid) %>%
-    filter(sum(!(str_split_1(word, '') %in% letterlist)) == 0) %>%
-    filter(grepl(middle, word)) 
+    filter(grepl(middle, word)) %>%
+    filter(sum(!(str_split_1(word, '') %in% letterlist)) == 0) 
   
-  validwords <- nrow(solutionlist)
-  pangrams <- sum(solutionlist$pangram)
-  print(validwords)
-  print(pangrams)
+  validwordcount <- nrow(solutionlist)
+  pangramcount <- sum(solutionlist$pangram)
+  print(validwordcount)
+  print(pangramcount)
   i <- i + 1
 }
